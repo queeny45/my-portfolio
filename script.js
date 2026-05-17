4a8f75981d123f18212aa92fca40b9136372c024
queeny45 <rayanmorkaz056@gmail.com>
2026-05-17 16:30:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// Typing Effect
+const text = "Computer Engineering Student • HR & Volunteer Experience • Tech Explorer";
+
+let i = 0;
+
+function typeEffect(){
+  if(i < text.length){
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 50);
+  }
+}
+
+typeEffect();
+
+
+// Scroll Reveal
+const sections = document.querySelectorAll(".section");
+
+function revealSections(){
+
+  const triggerBottom = window.innerHeight * 0.85;
+
+  sections.forEach(section => {
+
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if(sectionTop < triggerBottom){
+      section.classList.add("show");
+    }
+
+  });
+}
+
+window.addEventListener("scroll", revealSections);
+
+revealSections();
+
+
+// Button Scroll
+function scrollToSection(id){
+  document.getElementById(id).scrollIntoView({
+    behavior:"smooth"
+  });
+}
